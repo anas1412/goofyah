abd7fb391808fef567268f36488581293ccfa13b
anas1412 <anasbassoumi@gmail.com>
2023-05-27 02:55:37 +0100

    Fixed crash after one poop

--- content.js
+++ content.js
@@ -1,21 +1,53 @@
+// Create a flag to track if sound has already played
+let soundPlayed = false;
+
 // Listen for changes in the DOM
-document.addEventListener("DOMSubtreeModified", function () {
-  var textNodes = document.evaluate(
-    "//text()",
-    document,
-    null,
-    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-    null
-  );
-
-  for (var i = 0; i < textNodes.snapshotLength; i++) {
-    var node = textNodes.snapshotItem(i);
-
-    // Check if the word "poop" exists in the text node
-    if (node.textContent.includes("poop")) {
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to play the sound effect
+  function playSound() {
+    if (!soundPlayed) {
       // Send a message to the background script
       chrome.runtime.sendMessage({ word: "poop" });
-      break;
+      soundPlayed = true;
     }
   }
+
+  // Function to check if the word "poop" exists in the text node
+  function checkForPoop(node) {
+    if (node.textContent.toLowerCase().includes("poop")) {
+      playSound();
+    }
+  }
+
+  // Observe changes in the DOM using a MutationObserver
+  const observer = new MutationObserver(function (mutationsList) {
+    for (const mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        for (const addedNode of mutation.addedNodes) {
+          if (addedNode.nodeType === Node.TEXT_NODE) {
+            checkForPoop(addedNode);
+          } else if (addedNode.nodeType === Node.ELEMENT_NODE) {
+            const textNodes = document.evaluate(
+              ".//text()[normalize-space()]",
+              addedNode,
+              null,
+              XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+              null
+            );
+
+            for (let i = 0; i < textNodes.snapshotLength; i++) {
+              const node = textNodes.snapshotItem(i);
+              checkForPoop(node);
+            }
+          }
+        }
+      }
+    }
+  });
+
+  // Start observing changes in the entire document
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
 });
